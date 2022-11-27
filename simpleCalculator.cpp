185eadcf579757dd2a18bbc77637006e542f6c1c
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-11-27 10:52:19 +0500

    initial

---
+++ simpleCalculator.cpp
@@ -0,0 +1,36 @@
+﻿#include "iostream"
+
+using namespace std;
+
+
+int main() {
+    double static num1, num2, result;
+    int static operation;
+
+    cout << "Hello! Enter 1st number: ";
+    cin >> num1;
+    cout << "Enter 2nd number: ";
+    cin >> num2;
+    cout << "Enter the operation\n1: add\n2: subdivide\n3: multiply\n4: divide\nChoice operation number: ";
+    cin >> operation;
+    switch (operation) {
+    case 1:
+        result = num1 + num2;
+        break;
+    case 2:
+        result = num1 - num2;
+        break;
+    case 3:
+        result = num1 * num2;
+        break;
+    case 4:
+        result = num1 / num2;
+        break;
+    default:
+        cout << "Error";
+        main();
+    }
+    cout << "Result: " << result << endl;
+
+    main();
+}
